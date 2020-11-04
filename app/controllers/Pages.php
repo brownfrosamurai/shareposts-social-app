f5c647f73c4965d4d9e9869e121151cb01217755
meduna femi <femimeduna@gmail.com>
2020-11-04 11:52:51 +0100

    mapping methods and parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,14 @@
 <?php
     class Pages {
         public function __construct() {
-            echo 'Pages loaded';
+            
+        }
+
+        public function index() {
+
+        }
+        
+        public function about($id) {
+            echo $id;
         }
     }
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -25,6 +25,25 @@ class Core {
 
         // Instantiate controller class
         $this->currentController = new $this->currentController;
+
+        //Check for second part of Url
+        if(isset($url[1])) {
+            // Check to see if method exist in controller 
+            if(method_exists($this->currentController, $url[1])){
+                $this->currentMethod = $url[1];
+
+                // Unset 1 index 
+                unset($url[1]);
+            }
+        }
+
+        // Get params 
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([
+            $this->currentController, $this->currentMethod], 
+        $this->params);
     }
 
     public function getUrl() {
